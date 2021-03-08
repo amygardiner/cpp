d8df583c45ddccd84b0049de6b7107a00f78b0e8
amy <amy_rebecca_g@hotmail.co.uk>
2021-03-08 20:15:10 +0000

    Iterator for satellite vector doesn't yet print

--- assignment_3.cpp
+++ assignment_3.cpp
@@ -18,15 +18,15 @@ private:
   double redshift;
   double total_mass;
   double stellar_mass_fraction;
-  //int satellite;
+  int satellites;
   std::vector<galaxy> satellite_data;
 
 public:
   // Constructors
-  galaxy() : hubble_type{"Irr"}, redshift{}, total_mass{}, stellar_mass_fraction{}  {}
+  galaxy() : hubble_type{"Irr"}, redshift{}, total_mass{}, stellar_mass_fraction{}, satellites{}  {}
 
-  galaxy(std::string g_hubble_type , double g_redshift , double g_total_mass , double g_stellar_mass_fraction) :
-   hubble_type{g_hubble_type}, redshift{g_redshift}, total_mass{g_total_mass}, stellar_mass_fraction{g_stellar_mass_fraction}
+  galaxy(std::string g_hubble_type , double g_redshift , double g_total_mass , double g_stellar_mass_fraction, int g_satellites) :
+   hubble_type{g_hubble_type}, redshift{g_redshift}, total_mass{g_total_mass}, stellar_mass_fraction{g_stellar_mass_fraction}, satellites{g_satellites}
   {}
   // Destructor
   ~galaxy() {}
@@ -48,7 +48,7 @@ public:
   // Add a satellite galaxy
   void add_satellite()
   {
-    galaxy s1("SBa,",5.0,1.e10,0.03);
+    galaxy s1("SBa,",5.0,1.e10,0.03,0);
     satellite_data.push_back(s1);
   }
 };
@@ -57,9 +57,14 @@ public:
 void galaxy::print_data()
 {
   std::cout.precision(3); 
-  std::cout<<"Galaxy/Satellite: [Hubble type,z,m,f_*] = ["<<hubble_type<<","<< redshift
-       <<","<<total_mass<<","<<stellar_mass_fraction<<"]"<<std::endl;
-  std::cout<<"Satellite number is: "<<satellite_data.size()<<std::endl;
+  std::cout<<"Galaxy: [Hubble type,z,m,f_*,no. satellites] = ["<<hubble_type<<","<< redshift
+       <<","<<total_mass<<","<<stellar_mass_fraction<<","<<satellites<<"]"<<std::endl;
+  if(satellites>0)
+    for(int i{};i<satellite_data.size();i++)
+      {
+        std::cout<<"Satellite: [Hubble type,z,m,f_*, no. satellites] = ["<<std::endl;
+        satellite_data[i].print_data();
+      }
   return;
 }
 
@@ -70,7 +75,7 @@ int main()
   galaxy g1; 
 
   // Example using parameterised constructor
-  galaxy g2("E2",1.0,3.e8,0.02);
+  galaxy g2("E2",1.0,3.e8,0.02,1);
 
   // Print out data
   std::vector<galaxy> galaxy_data;
@@ -83,8 +88,9 @@ int main()
   }
   
   // Change Hubble type from Irr to S0
-  //g1.change_type("S0");
-  galaxy_data[1].change_type("S0");
+  galaxy_data[0].change_type("S0");
+
+  std::cout<<"Changing Irr to S0:"<<std::endl;
 
   for(auto galaxy_it=galaxy_data.begin(); galaxy_it <galaxy_data.end(); ++galaxy_it){
     galaxy_it->print_data();
