6d32ced7b03006091eb66b1f953ff1729b9d0779
amy <amy_rebecca_g@hotmail.co.uk>
2021-03-23 10:54:48 +0000

    Getting started

---
+++ assignment_5.cpp
@@ -0,0 +1,33 @@
+// PHYS 30762 Programming in C++
+// Assignment 5
+// Amy Gardiner 10137582
+// Program for matrices
+
+#include<iostream>
+#include<cmath>
+
+class matrix
+{
+  private:
+    int rows;
+    int columns;
+    double* data;
+  public:
+    //matrix(double matrix_data, double no_rows, double no_columns)
+    matrix(double no_rows, double no_columns)
+    {
+        rows=no_rows; 
+        columns=no_columns;
+        data=new double[(rows+1)+(columns+1)];
+    }
+    ~matrix()
+    { 
+        delete []data;
+    }
+};
+
+int main()
+{ 
+    matrix a{3,4};
+    return 0;
+}
