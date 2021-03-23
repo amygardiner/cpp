7ee639666dda256040215fb1ca45d21cac517ffd
amy <amy_rebecca_g@hotmail.co.uk>
2021-03-23 11:35:06 +0000

    Changed constructor style

--- assignment_5.cpp
+++ assignment_5.cpp
@@ -9,24 +9,32 @@
 class matrix
 {
   private:
-    int rows;
-    int columns;
-    double* data;
+    size_t rows{};
+    size_t columns{};
+    double* array{nullptr};
   public:
-    matrix(double no_rows, double no_columns)
-    {
-        rows=no_rows; 
-        columns=no_columns;
-        data=new double[(rows+1)*(columns+1)];
-    }
+    matrix(size_t no_rows, size_t no_columns);
     ~matrix()
     { 
-        delete []data;
+        delete array;
     }
 };
-
+matrix::matrix(size_t no_rows, size_t no_columns)
+{
+    if(no_rows<1||no_columns<1)
+    {
+        std::cout<<"The array must have positive size"<<std::endl;
+        throw("size not positive");
+    }
+    rows=no_rows;
+    columns=no_columns;
+    size_t length=(rows+1)*(columns+1);
+    array=new double[length];
+    for(size_t i{};i<length;i++) array[i]=1;
+}
 int main()
 { 
     matrix a{3,4};
+    matrix b{3,4};
     return 0;
 }
