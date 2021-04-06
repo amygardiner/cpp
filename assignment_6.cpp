aa2e8102e963a57f1ad691da31213182df95e48d
amy <amy_rebecca_g@hotmail.co.uk>
2021-04-06 12:38:12 +0100

    Incorporating virtual functions and overriding

--- assignment_6.cpp
+++ assignment_6.cpp
@@ -18,11 +18,10 @@ public:
     shape(const std::string nm) : name{nm} {}
     // Destructor
     ~shape(){}
-    std::string shape_name()
+    virtual void info()
     {
-        return name;
+        std::cout<<name<<std::endl;
     }
-    friend std::ostream& operator<<(std::ostream&, const shape&); 
 };
 
 class rectangle: public shape
@@ -34,27 +33,38 @@ public:
     rectangle() : shape{}, length{}, width{} {}
     rectangle(const std::string nm, const double l, const double w) : shape{nm}, length{l}, width{w} {}
     ~rectangle(){}
-    double area() const
+    double area{length*width};
+    void info()
     {
-        return length*width;
+        std::cout<<name<<" has area = "<<area<<std::endl;
     }
-    friend std::ostream& operator<<(std::ostream&, const rectangle&);
 };
 
-std::ostream& operator<<(std::ostream& o, const rectangle& re)
-{
-    o<<"Area of rectangle = "<<re.area()<<std::endl;
-    return o;
-}
-
-std::ostream& operator<<(std::ostream& o, const shape& sh)
-{
-    o<<"Shape: "<<sh.name<<std::endl;
-    return o;
-}
 int main() 
 {
-    rectangle rect1{"rectangle1", 2, 3};
-    std::cout<<rect1;
+    shape rect1{};
+    rectangle rect2{"Rectangle", 2, 3};
+    shape *shape_pointer;
+    shape_pointer=&rect1;
+    shape_pointer->info();
+    shape_pointer=&rect2;
+    shape_pointer->info();
+
+/* For when all derived classes are made
+    shape *shape_array[8];
+    shape_array[0]=new shape{};
+    shape_array[1]=new rectangle{"Rectangle",2,3};
+    shape_array[2]=new square{"Square",2};
+    shape_array[3]=new ellipse{"Ellipse",2,3};
+    shape_array[4]=new circle{"Circle",2,3};
+    shape_array[5]=new cuboid{"Cuboid",2,3};
+    shape_array[6]=new cube{"Cube",2};
+    shape_array[7]=new ellipsoid{"Ellipsoid",2,3};
+    shape_array[8]=new sphere{"Sphere",2};
+    for(int i{0};i<=8;i++)
+        shape_array[i]->info();
+        delete shape_array[i];
+        shape_array[i]=0;
+*/
     return 0;
 }
