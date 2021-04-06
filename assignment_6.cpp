026eb894c5d65e28ad2e7bcae9aecff5eb339606
amy <amy_rebecca_g@hotmail.co.uk>
2021-04-06 13:39:55 +0100

    All derived classes functional

--- assignment_6.cpp
+++ assignment_6.cpp
@@ -3,8 +3,11 @@
 // Amy Gardiner 10137582
 // Program for 2D and 3D shapes
 
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <string>
+#include<cmath>
 
 // Base class for shapes
 class shape
@@ -37,27 +40,131 @@ public:
     }
 };
 
+class square: public shape
+{
+protected:
+    double length;
+public:
+    square() : shape{}, length{} {}
+    square(const std::string nm, const double l) : shape{nm}, length{l} {}
+    ~square(){}
+    double area{pow(length,2)};
+    void info()
+    {
+        std::cout<<name<<" has area = "<<area<<std::endl;
+    }
+};
+
+class ellipse: public shape
+{
+protected:
+    double axis1;
+    double axis2;
+public:
+    ellipse() : shape{}, axis1{}, axis2{} {}
+    ellipse(const std::string nm, const double a, const double b) : shape{nm}, axis1{a}, axis2{b} {}
+    ~ellipse(){}
+    double area{M_PI*axis1*axis2};
+    void info()
+    {
+        std::cout<<name<<" has area = "<<area<<std::endl;
+    }
+};
+
+class circle: public shape
+{
+protected:
+    double radius;
+public:
+    circle() : shape{}, radius{} {}
+    circle(const std::string nm, const double r) : shape{nm}, radius{r} {}
+    ~circle(){}
+    double area{M_PI*pow(radius,2)};
+    void info()
+    {
+        std::cout<<name<<" has area = "<<area<<std::endl;
+    }
+};
+
+class cuboid: public shape
+{
+protected:
+    double width;
+    double length;
+    double height;
+public:
+    cuboid() : shape{}, width{}, length{}, height{} {}
+    cuboid(const std::string nm, const double w, const double l, const double h) : shape{nm}, width{w}, length{l}, height{h} {}
+    ~cuboid(){}
+    double volume{width*length*height};
+    void info()
+    {
+        std::cout<<name<<" has volume = "<<volume<<std::endl;
+    }
+};
+
+class cube: public shape
+{
+protected:
+    double length;
+public:
+    cube() : shape{}, length{} {}
+    cube(const std::string nm, const double l) : shape{nm}, length{l} {}
+    ~cube(){}
+    double volume{pow(length,3)};
+    void info()
+    {
+        std::cout<<name<<" has volume = "<<volume<<std::endl;
+    }
+};
+
+class ellipsoid: public shape
+{
+protected:
+    double axis1;
+    double axis2;
+    double axis3;
+public:
+    ellipsoid() : shape{}, axis1{}, axis2{}, axis3{} {}
+    ellipsoid(const std::string nm, const double a, const double b, const double c) : shape{nm}, axis1{a}, axis2{b}, axis3{c} {}
+    ~ellipsoid(){}
+    double volume{(4.0/3.0)*M_PI*(axis1*axis2*axis3)};
+    void info()
+    {
+        std::cout<<name<<" has volume = "<<volume<<std::endl;
+    }
+};
+
+class sphere: public shape
+{
+protected:
+    double radius;
+public:
+    sphere() : shape{}, radius{} {}
+    sphere(const std::string nm, const double r) : shape{nm}, radius{r} {}
+    ~sphere(){}
+    double volume{(4.0/3.0)*M_PI*pow(radius,3)};
+    void info()
+    {
+        std::cout<<name<<" has volume = "<<volume<<std::endl;
+    }
+};
+
 int main() 
 {
-    rectangle rect1{"Rectangle", 2, 3};
-    shape *shape_pointer;
-    shape_pointer=&rect1;
-    shape_pointer->info();
-
-/* For when all derived classes are made
     shape *shape_array[7];
     shape_array[0]=new rectangle{"Rectangle",2,3};
     shape_array[1]=new square{"Square",2};
     shape_array[2]=new ellipse{"Ellipse",2,3};
-    shape_array[3]=new circle{"Circle",2,3};
-    shape_array[4]=new cuboid{"Cuboid",2,3};
+    shape_array[3]=new circle{"Circle",5};
+    shape_array[4]=new cuboid{"Cuboid",1,2,3};
     shape_array[5]=new cube{"Cube",2};
-    shape_array[6]=new ellipsoid{"Ellipsoid",2,3};
+    shape_array[6]=new ellipsoid{"Ellipsoid",1,2,3};
     shape_array[7]=new sphere{"Sphere",2};
+    int i{};
     for(int i{0};i<=7;i++)
         shape_array[i]->info();
         delete shape_array[i];
         shape_array[i]=0;
-*/
     return 0;
 }
